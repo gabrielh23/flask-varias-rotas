d938712c5cd508bac1ae99d395bc127b3e6bfab6
Gabriel Henrique Gomes Firmino <40707824+gabrielh23@users.noreply.github.com>
2022-03-11 00:30:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def hello_world():
+  return '<h1>Olá Mundo!</h1>'
+
+@app.route('/unifran')
+def unifran():
+  return '<h2>Universidade de Franca</h2>'
+
+@app.route('/dashboard/<nome>')
+def nome(nome):
+  return f'Olá, {nome}'
+
+app.run(host='0.0.0.0', port=8080)
